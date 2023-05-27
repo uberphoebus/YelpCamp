4358ade4927af06de40c0d2a511c6e7789615c9d
uberphoeus <uberphoeus@gmail.com>
2023-05-28 08:57:35 +0900

    add new form

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ db.once("open", () => {
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // functions
 const logRoutes = (req, res, action = "") => {
@@ -44,6 +45,18 @@ app.get("/campgrounds", async (req, res) => {
     logRoutes(req, res);
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    logRoutes(req, res);
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    logRoutes(req, res, `posted ${campground.title}`);
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
