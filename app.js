1800cf1b4ae010d5a6af42d96043bb2cb25b229d
uberphoeus <uberphoeus@gmail.com>
2023-05-28 13:54:02 +0900

    add images show

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const env = require("dotenv");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const morgan = require("morgan");
+const colors = require("colors");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -17,19 +19,23 @@ mongoose.connect(process.env.URI, {
 const db = mongoose.connection;
 db.on(
     "error",
-    console.error.bind(console, `[${"mongo".padEnd(7)}] connection error`)
+    console.error.bind(
+        console,
+        `[${"mongo".padEnd(7)}] connection error`.brightMagenta
+    )
 );
 db.once("open", () => {
-    console.log(`[${"mongo".padEnd(7)}] database connected`);
+    console.log(`[${"mongo".padEnd(7)}] database connected`.brightMagenta);
 });
 
-// app config
+// app/middleware config
 const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 // functions
 const logRoutes = (req, res, action = "") => {
@@ -40,36 +46,30 @@ const logRoutes = (req, res, action = "") => {
 
 // routes
 app.get("/", (req, res) => {
-    logRoutes(req, res);
     res.render("home");
 });
 
 app.get("/campgrounds", async (req, res) => {
-    logRoutes(req, res);
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
 });
 
 app.get("/campgrounds/new", (req, res) => {
-    logRoutes(req, res);
     res.render("campgrounds/new");
 });
 
 app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    logRoutes(req, res, `posted ${campground.title}`);
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
-    logRoutes(req, res);
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
 });
 
 app.get("/campgrounds/:id/edit", async (req, res) => {
-    logRoutes(req, res);
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/edit", { campground });
 });
@@ -79,18 +79,16 @@ app.put("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, {
         ...req.body.campground,
     });
-    logRoutes(req, res, `updated ${campground.title}`);
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.delete("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    logRoutes(req, res, `deleted ${id}`);
     res.redirect("/campgrounds");
 });
 
 // app listen
 app.listen(3000, () => {
-    console.log(`[${"server".padEnd(7)}] on port 3000`);
+    console.log(`[${"server".padEnd(7)}] on port 3000`.brightCyan);
 });
