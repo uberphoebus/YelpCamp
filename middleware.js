7ecb66396274a0dd507815706e4e3668c60886fb
uberphoeus <uberphoeus@gmail.com>
2023-05-28 10:50:18 +0900

    add middleware path

--- middleware.js
+++ middleware.js
@@ -8,6 +8,10 @@ app.use((req, res, next) => {
     console.log(req.method, req.path);
     next();
 });
+app.use("/dogs", (req, res, next) => {
+    console.log("I love dogs");
+    next();
+});
 // app.use((req, res, next) => {
 //     console.log("this is my first middleware");
 //     next();
@@ -30,6 +34,10 @@ app.get("/", (req, res) => {
 app.get("/dogs", (req, res) => {
     console.log(`request date: ${req.requestTime}`);
     res.send("woof woof");
+});
+
+app.use((req, res) => {
+    res.status(404).send("NOT FOUND!");
 });
 
 app.listen(3001, () => {
