a8f446bc1574674213435974f26b32ee2475a24d
uberphoeus <uberphoeus@gmail.com>
2023-05-28 10:33:43 +0900

    add middleware decorators

--- middleware.js
+++ middleware.js
@@ -4,20 +4,31 @@ const morgan = require("morgan");
 
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-    console.log("this is my first middleware");
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
     next();
-    console.log("this is my first middleware after calling next()");
 });
-app.use((req, res, next) => {
-    console.log("this is my second middleware");
-    return next();
-});
+// app.use((req, res, next) => {
+//     console.log("this is my first middleware");
+//     next();
+//     console.log("this is my first middleware after calling next()");
+// });
+// app.use((req, res, next) => {
+//     console.log("this is my second middleware");
+//     return next();
+// });
+// app.use((req, res, next) => {
+//     console.log("this is my third middleware");
+//     return next();
+// });
 
 app.get("/", (req, res) => {
+    console.log(`request date: ${req.requestTime}`);
     res.send("home page");
 });
 
 app.get("/dogs", (req, res) => {
+    console.log(`request date: ${req.requestTime}`);
     res.send("woof woof");
 });
 
