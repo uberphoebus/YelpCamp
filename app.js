d3ed76ae4f5ad7ff7c1183789cfefd0b8bde1fda
uberphoeus <uberphoeus@gmail.com>
2023-05-28 11:12:38 +0900

    add ejs layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const env = require("dotenv");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -24,6 +25,7 @@ db.once("open", () => {
 
 // app config
 const app = express();
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
