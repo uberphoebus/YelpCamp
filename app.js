e46dbaa918ab37c45fdb8b80168bcaee9165e625
uberphoeus <uberphoeus@gmail.com>
2023-05-29 14:40:55 +0900

    add delete route

--- app.js
+++ app.js
@@ -138,6 +138,18 @@ app.post(
     })
 );
 
+app.delete(
+    "/campgrounds/:id/reviews/:reviewId",
+    catchAsync(async (req, res) => {
+        const { id, reviewId } = req.params;
+        await Campground.findByIdAndUpdate(id, {
+            $pull: { reviews: reviewId },
+        });
+        await Review.findByIdAndDelete(req.params.reviewId);
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+
 // error handling
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page Not Found", 404));
