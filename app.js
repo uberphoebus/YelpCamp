8aa41e589bb5fe23e53e49c23c68b0a1c8f8f559
uberphoeus <uberphoeus@gmail.com>
2023-05-29 12:19:28 +0900

    add review model and form

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const morgan = require("morgan");
 const colors = require("colors");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 // database config
 env.config();
@@ -109,6 +110,18 @@ app.delete(
         const { id } = req.params;
         await Campground.findByIdAndDelete(id);
         res.redirect("/campgrounds");
+    })
+);
+
+app.post(
+    "/campgrounds/:id/reviews",
+    catchAsync(async (req, res) => {
+        const campground = await Campground.findById(req.params.id);
+        const review = new Review(req.body.review);
+        campground.reviews.push(review);
+        await review.save();
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
     })
 );
 
