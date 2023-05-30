bcbf0a09a19f766ce42185f1a55da5ddd6f57a8e
uberphoeus <uberphoeus@gmail.com>
2023-05-30 23:27:20 +0900

    add auths

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,7 +61,12 @@ router.get(
     "/:id/edit",
     isLoggedIn,
     catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id);
+        const { id } = req.params;
+        const campground = await Campground.findById(id);
+        if (!campground.author.equals(req.user._id)) {
+            req.flash("error", "You do not have permission to do that!");
+            return res.redirect(`/campgrounds/${id}`);
+        }
         if (!campground) {
             req.flash("error", "Cannot find that campground!");
             return res.redirect("/campgrounds");
@@ -76,7 +81,12 @@ router.put(
     validateCampground,
     catchAsync(async (req, res) => {
         const { id } = req.params;
-        const campground = await Campground.findByIdAndUpdate(id, {
+        const campground = await Campground.findById(id);
+        if (!campground.author.equals(req.user._id)) {
+            req.flash("error", "You do not have permission to do that!");
+            return res.redirect(`/campgrounds/${id}`);
+        }
+        const camp = await Campground.findByIdAndUpdate(id, {
             ...req.body.campground,
         });
         req.flash("success", "Successfully updated campground!");
@@ -89,7 +99,14 @@ router.delete(
     isLoggedIn,
     catchAsync(async (req, res) => {
         const { id } = req.params;
-        await Campground.findByIdAndDelete(id);
+        const campground = await Campground.findById(id);
+        if (!campground.author.equals(req.user._id)) {
+            req.flash("error", "You do not have permission to do that!");
+            return res.redirect(`/campgrounds/${id}`);
+        }
+        const camp = await Campground.findByIdAndUpdate(id, {
+            ...req.body.campground,
+        });
         req.flash("success", "Successfully deleted campground!");
         res.redirect("/campgrounds");
     })
