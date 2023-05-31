74951eaed6aff9f8d04748539acc938f338bd545
uberphoeus <uberphoeus@gmail.com>
2023-05-31 22:28:33 +0900

    add uploaded img show

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,15 +13,12 @@ const upload = multer({ storage });
 router
     .route("/")
     .get(catchAsync(campgrounds.index))
-    .post(upload.array("image"), (req, res) => {
-        console.log(req.body, req.files);
-        res.send("It worked!");
-    });
-// .post(
-//     isLoggedIn,
-//     validateCampground,
-//     catchAsync(campgrounds.createCampground)
-// );
+    .post(
+        isLoggedIn,
+        upload.array("image"),
+        validateCampground,
+        catchAsync(campgrounds.createCampground)
+    );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
