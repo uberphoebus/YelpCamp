bac2d31c5d3edaf53fa704253fcb7555b4622e10
uberphoeus <uberphoeus@gmail.com>
2023-06-01 19:34:21 +0900

    add images to db

--- schemas.js
+++ schemas.js
@@ -5,7 +5,7 @@ module.exports.campgroundSchema = Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string(),
-        // image: Joi.string().required(),
+        image: Joi.string(),
         location: Joi.string().required(),
         description: Joi.string().required(),
     }).required(),
